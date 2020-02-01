1b5c63dcbcfd01b195c4ea7fc5518a546e20b988
septynade <septynade@gmail.com>
2020-02-01 18:14:04 +0700

    init commit

---
+++ src/components/Header.js
@@ -0,0 +1,16 @@
+import React from "react";
+import EventNoteIcon from '@material-ui/icons/EventNote';
+
+
+function Header() {
+    return (
+        <header>
+            <h1>
+                <EventNoteIcon />
+                Note It!
+            </h1>
+        </header>
+    );
+}
+
+export default Header;
