bf164fd7d8e5b632afddce6f84723d0abae72816
septynade <septynade@gmail.com>
2020-02-01 18:13:42 +0700

    Updates

---
+++ precache-manifest.8a4b507ef4a587442fc12e6b1a41f970.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c938106a2267c3890e212e1eb64dc4d0",
+    "url": "/noteit/index.html"
+  },
+  {
+    "revision": "055259f49bac1f6586a4",
+    "url": "/noteit/static/css/main.a3e22d32.chunk.css"
+  },
+  {
+    "revision": "4e217255778592e9e39e",
+    "url": "/noteit/static/js/2.0198b11e.chunk.js"
+  },
+  {
+    "revision": "e2fc3609d47d90b8cc004bfe15ddf15e",
+    "url": "/noteit/static/js/2.0198b11e.chunk.js.LICENSE"
+  },
+  {
+    "revision": "055259f49bac1f6586a4",
+    "url": "/noteit/static/js/main.88283ac6.chunk.js"
+  },
+  {
+    "revision": "b9f844274971d6d7d77d",
+    "url": "/noteit/static/js/runtime-main.452bdc43.js"
+  }
+]);
